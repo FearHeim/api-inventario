f7cf48a47dc430f1a87ed94e9389be20280a6b9f
José Palacio <53056146+FearHeim@users.noreply.github.com>
2021-08-19 11:29:51 -0500

    Completar CRUD

--- src/index.js
+++ src/index.js
@@ -30,15 +30,53 @@ app.get("/api/v1", (req, res) => {
 
 app.get("/api/v1/inventario", async (req, res) => {
   const items = await Item.find();
-  res.json(items);
+  res.send(items);
 });
 
 app.post("api/v1/nuevo_item", async (req, res) => {
-  const newItem = new Item(arg);
-  const ItemSaved = await newItem.save();
-  console.log(ItemSaved);
-  res.send("new-Item-created", JSON.stringify(ItemSaved));
+  const item = new Item({
+    Nom_producto: req.body.Nom_producto,
+    Des_producto: req.body.Des_producto,
+    Pre_producto: req.body.Pre_producto
+  });
+  await item.save();
+  res.send(item);
 });
+
+app.delete("api/v1/delete-item", async (req, res)=>{
+  try {
+    await Item.deleteOne({ _id: req.params.id });
+    res.status(204).send();
+  } catch {
+    res.status(404);
+    res.send({ error: "Item doesn't exist!" });
+  }
+})
+
+
+app.patch("api/v1/update", async (req, res)=>{
+  try {
+		const item = await Item.findOne({ _id: req.params.id })
+
+		if (req.body.Pre_producto) {
+			item.Pre_producto = req.body.Pre_producto
+		}
+
+		if (req.body.Des_producto) {
+			post.Des_producto = req.body.Des_producto
+		}
+    
+		if (req.body.Pre_producto) {
+			post.Pre_producto = req.body.Pre_producto
+		}
+
+		await item.save()
+		res.send(item)
+	} catch {
+		res.status(404)
+		res.send({ error: "item doesn't exist!" })
+	}
+})
 
 let port = process.env.PORT;
 let server = app.listen(port, () => {
